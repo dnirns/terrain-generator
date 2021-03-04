cdf466316e5034d25d4ea148c0117fc64608f453
dnirns <daniel.irons@gmail.com>
2021-03-04 13:57:54 +0000

    song and play button added

--- src/sketch/index.js
+++ src/sketch/index.js
@@ -3,7 +3,7 @@ import 'p5/lib/addons/p5.sound'
 import * as p5 from 'p5'
 
 const sketch = (p) => {
-  const w = window.innerWidth
+  const w = window.innerWidth * 1.5
   const h = window.innerHeight
   let rows = 0
   let cols = 0
@@ -11,65 +11,96 @@ const sketch = (p) => {
   let terrain = []
   let flyingSpeed = 0
 
-  let speedSlider
-  let heightSlider
-  let depthSlider
+  // let speedSlider
+  // let heightSlider
+  // let depthSlider
 
-  let mic
+  //*variable for the audio source
+  let sound
+  let startButton
+  let fft
+  p.preload = function preload() {
+    //*preload mp3 as the audio source for p5 sound to reference
+    // sound = p.loadSound('song.mp3')
+  }
+
   p.setup = function setup() {
     p.createCanvas(w, h, p.WEBGL)
+    sound = p.loadSound('song.mp3')
+    //*assign amplitude as a new instance of the p5 Amplitude function
+    fft = new p5.FFT()
 
-    //*audio in
+    //*analyze frequency with Fast Fourier Transform algorythm
 
-    mic = new p5.AudioIn()
-    mic.start()
+    //*play sound source
+    // sound.play()
+
     //*set number of columns and rows to width / height divided by the scale of each triangle
+
     cols = w / scale
     rows = h / scale
 
+    startButton = p.createButton('PLAY')
+    startButton.position(10, 10)
+    startButton.mousePressed(playSound)
     //*sliders for manipulating the visuals
-    speedSlider = p.createSlider(0, 0.2, 0.004, 0)
-    speedSlider.position(10, 10)
-    speedSlider.style('width', '300px')
+    // speedSlider = p.createSlider(0, 0.2, 0.004, 0)
+    // speedSlider.position(10, 10)
+    // speedSlider.style('width', '300px')
 
-    heightSlider = p.createSlider(-350, 0, -180, 0)
-    heightSlider.position(10, 30)
-    heightSlider.style('width', '300px')
+    // heightSlider = p.createSlider(-350, 0, -180, 0)
+    // heightSlider.position(10, 30)
+    // heightSlider.style('width', '300px')
 
-    depthSlider = p.createSlider(0, 350, 50, 0)
-    depthSlider.position(10, 50)
-    depthSlider.style('width', '300px')
+    // depthSlider = p.createSlider(0, 350, 50, 0)
+    // depthSlider.position(10, 50)
+    // depthSlider.style('width', '300px')
+
+    //*set audio source volume
   }
 
+  const playSound = () => {
+    if (sound.isPlaying()) {
+      sound.stop()
+    } else {
+      sound.play()
+    }
+  }
   p.draw = function draw() {
-    flyingSpeed -= speedSlider.value()
-    let yoff = flyingSpeed
+    //*get analysis from the FFT algorythm
+    // fft.smooth(0.7)
+    fft.analyze()
 
+    //*get treble and bass energy values from result of analysis
+    let treble = fft.getEnergy('treble')
+    let bass = fft.getEnergy('bass')
+
+    //*assign energy values to variables to be used in terrain generation
+    let depth = -bass * 1.5
+    let height = treble * 2.2
+
+    //*alter flyingspeed based on value of slider input
+    flyingSpeed -= 0.008
+    let yoff = flyingSpeed
     for (let y = 0; y < rows; y++) {
       let xoff = 0
       terrain[y] = []
 
       for (let x = 0; x < cols; x++) {
-        terrain[y][x] = p.map(
-          p.noise(xoff, yoff),
-          0,
-          1,
-          heightSlider.value(),
-          depthSlider.value()
-        )
+        terrain[y][x] = p.map(p.noise(xoff, yoff), 0, 1, height, depth)
         xoff += 0.1
       }
       yoff += 0.1
     }
-    p.background(220)
+    p.background('rgb(163, 204, 254)')
     p.stroke(255)
 
     p.fill(0)
 
     //* rotate sketch along x axis
-    p.rotateX(p.PI / 3.5)
+    p.rotateX(p.PI / 4)
     //* move sketch to align center after the rotation
-    p.translate(-w / 2, -h / 2 + 50)
+    p.translate(-w / 1.4, -h / 2 + 40)
     //* loop to draw grid of triangles
     for (let y = 0; y < rows - 1; y++) {
       p.beginShape(p.TRIANGLE_STRIP)
